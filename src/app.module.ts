88dbb2a5e6ff61cb319a39af967419e45446714c
daheeahn <deg9810@gmail.com>
2021-02-08 14:51:00 +0900

    # 2.6 Modules and Dependency Injection (07:07)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { MoviesController } from './movies/movies.controller';
-import { MoviesService } from './movies/movies.service';
+import { MoviesModule } from './movies/movies.module';
+import { AppController } from './app.controller';
 
 @Module({
-  imports: [],
-  controllers: [MoviesController],
-  providers: [MoviesService],
+  imports: [MoviesModule], // 이 안에 movies의 controller, provider 가 있다. movies 말고 다른 모듈도 생기면 이 안에!
+  controllers: [AppController], // nest g co, app (그리고 app.controller.ts를 src로 옮긴다. 루트 컨트롤러지.)
+  providers: [],
 })
 export class AppModule {}
